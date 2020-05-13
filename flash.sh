6842b6b582f938a36eb92b8b285780608eeeba8d
Dmitry Kudryavtsev <ko.dmitri@gmail.com>
2020-05-13 19:58:47 +0300

    fixes

--- flash.sh
+++ flash.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 sudo dfu-programmer atmega32u4 erase
-sudo dfu-programmer atmega32u4 flash dz60_skwee357.hex
+sudo dfu-programmer atmega32u4 flash dz60.hex
 sudo dfu-programmer atmega32u4 reset
 
